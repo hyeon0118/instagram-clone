50addf49cc797e4ab45249c44f63f7f757ac47e9
hyeon0118 <hyeon0118@gmail.com>
2022-11-22 14:54:10 +0100

    Template Tag update

--- content/views.py
+++ content/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .models import Feed
+
 
 # Create your views here.
+class Main(APIView):
+    def get(self, request):
+        feed_list = Feed.objects.all().order_by('-id')
+        return render(request, "hyeonstagram/main.html", context=dict(feed_list=feed_list))
