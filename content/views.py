b705895007b4887ec4122a2cd106414e2e6e2b19
hyeon0118 <hyeon0118@gmail.com>
2022-11-26 17:09:32 +0100

    feed upload API update

--- content/views.py
+++ content/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from rest_framework.response import Response
 from .models import Feed, User
+from uuid import uuid4
+import os
+from instagram_clone.settings import MEDIA_ROOT
 
 
 # Create your views here.
@@ -8,3 +12,23 @@ class Main(APIView):
     def get(self, request):
         feed_list = Feed.objects.all().order_by('-id')
         return render(request, "hyeonstagram/main.html", context=dict(feed_list=feed_list))
+
+
+class UploadFeed(APIView):
+    def post(self, request):
+        file = request.FILES["file"]
+        uuid_name = uuid4().hex
+        save_path = os.path.join(MEDIA_ROOT, uuid_name)
+
+        with open(save_path, 'wb+') as destination:
+            for chunk in file.chunks():
+                destination.write(chunk)
+
+        image = uuid_name
+        content = request.data.get("content")
+        user_id = request.data.get("user_id")
+        profile_image = request.data.get("profile_image")
+
+        Feed.objects.create(image=image, content=content, user_id=user_id, profile_image=profile_image, like_count=0)
+
+        return Response(status=200)
--- instagram_clone/settings.py
+++ instagram_clone/settings.py
@@ -12,10 +12,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -27,7 +27,6 @@ SECRET_KEY = config("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -74,7 +73,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "instagram_clone.wsgi.application"
 
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
@@ -84,7 +82,6 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -104,7 +101,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -116,7 +112,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
@@ -126,3 +121,10 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# URL Prefix for each media files
+MEDIA_URL = '/media/'
+# MEDIA_URL = 'http://static.myservice.com/media/' if a media file is copied to the other server
+
+# path to a directory where uploaded file will be stored
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
